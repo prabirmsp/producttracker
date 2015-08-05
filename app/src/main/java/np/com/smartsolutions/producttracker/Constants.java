6b6a8d97f41fb59a1b7243e06491637d235196ce
Prabir Pradhan <prabirmsp@gmail.com>
2015-08-05 12:25:41 +0545

    apk made

--- app/src/main/java/np/com/smartsolutions/producttracker/Constants.java
+++ app/src/main/java/np/com/smartsolutions/producttracker/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
 
     // Connection
     //public static final String URL = "http://172.16.31.236/test/tracker.php";
-    public static final String URL = "http://smartsolutions.com.np/dev/prodtrak/pt.php";
+    public static final String URL = "http://smartsolutions.com.np/dev/prodtrak/tracker.php";
     //public static final String URL = "http://192.168.0.110/tracker.php";
     //public static final String URL = "http://192.168.43.34/tracker.php";
 
